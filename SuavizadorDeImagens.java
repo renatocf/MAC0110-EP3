974c52f46684c990dfedf791b005d8c50b1932c5
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2012-06-04 18:43:54 -0300

    Adding "SuavizadorDeImagens", the future class that will allow the interaction between user and the other classes

---
+++ SuavizadorDeImagens.java
@@ -0,0 +1,17 @@
+class SuavizadorDeImagens
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Menu");
+    System.out.println("====");
+    System.out.println("1) Ler imagem");
+    System.out.println("2) Visualizar imagem");
+    System.out.println("3) Suavizar com filtro médio");
+    System.out.println("4) Suavizar com filtro mediano");
+    System.out.println("5) Suavizar com filtro gaussiano");
+    System.out.println("6) Gravar imagem");
+    System.out.println("7) Fim");
+                         
+                         
+  } // main
+} // class
